Fixed mayor bug in the loading data preventing us from plotting data collected directly to gdata. Added averaged interferometry calculations. Fixed additional bugs and improved behavior of functions.

mictools/peak_modelling.py
@@ -54,136 +54,6 @@ def fit_scan(scanno, xcol, ycol, normcol=None, visualize = False,
     
     return output
 
-# def graph_run(scans, xcol, ycol, zcol=None, normcol=None):
-        
-#     # if type(ycol) == dict:
-#     #     ycolName = 'Intensity'
-#     # else:
-#     #     ycolName = ycol
-    
-#     dataFrames = []
-    
-#     for scanno in scans:
-        
-#         xdata, ydata, output = fit_scan(scanno, xcol=xcol, ycol=ycol,
-#                                                 normcol=normcol, getData=True)
-        
-#         baseline = load_scan(scanno, stream_name="baseline")
-#         zval= baseline[zcol].mean()
-#         zdata = zval*np.ones(len(ydata))
-#         frame = pd.DataFrame(data={xcol: xdata, ycol: ydata, zcol:zdata}) 
-#         dataFrames.append(frame)
-        
-        
-#     dataFrame = pd.concat(dataFrames)
-    
-#     plasmaColors = pc.sample_colorscale(pc.sequential.Plasma,
-#                             [n/(len(scans)-1) for n in range(len(scans))])
-    
-#     fig = px.line(dataFrame,x=xcol, y=ycol, color=zcol, markers=True,
-#             color_discrete_sequence = plasmaColors)
-        
-    
-#     fig.show()
-
-# def analyze_run(scans, xcol, ycol, zcol, normcol=None, 
-#                    paramsFrame=False):
-        
-#         # if type(ycol) == dict:
-#         #     ycolName = 'Intensity'
-#         # else:
-#         #     ycolName = ycol
-        
-#         dataFrames = []
-#         fitFrames = []
-#         paramFrame = pd.DataFrame()
-#         model = lmfit.models.PseudoVoigtModel() + lmfit.models.LinearModel()
-        
-#         for scanno in scans:
-            
-#             xdata, ydata, output = fit_scan(scanno, xcol=xcol, ycol=ycol,
-#                                                   normcol=normcol, getData=True)
-            
-
-#             baseline = load_scan(scanno, stream_name="baseline")
-#             zval= baseline[zcol].mean()
-#             zdata = zval*np.ones(len(ydata))
-#             frame = pd.DataFrame(data={xcol: xdata, ycol: ydata, zcol:zdata}) 
-#             dataFrames.append(frame)
-            
-#             xdatanew = np.linspace(xdata[0], xdata[-1], len(xdata)*10)
-#             ydatanew = model.eval(params=output.params, x=xdatanew)
-#             zdatanew = zval*np.ones(len(ydatanew))
-#             frameFit = pd.DataFrame(data={xcol: xdatanew, ycol + ' fit': ydatanew, 
-#                                           zcol:zdatanew}) 
-#             fitFrames.append(frameFit)
-            
-#             paramFrame.loc[scanno, zcol] = zval
-#             for param in output.params:
-#                 paramFrame.loc[scanno, param] = output.params[param].value
-                
-                
-#         if paramsFrame:
-#             return paramFrame
-        
-#         #Left panel with raw data and fit graph
-            
-#         dataFrame = pd.concat(dataFrames)
-#         fitFrame = pd.concat(fitFrames)
-        
-#         plasmaColors = pc.sample_colorscale(pc.sequential.Plasma,
-#                                 [n/(len(scans)-1) for n in range(len(scans))])
-        
-#         fig = px.line(dataFrame,x=xcol, y=ycol, color=zcol, markers=True,
-#                 color_discrete_sequence = plasmaColors)
-#         for data in fig.data:
-#             data.mode = 'markers'
-            
-#         fig2 = px.line(fitFrame,x=xcol, y=ycol+' fit', color=zcol, 
-#                 color_discrete_sequence = plasmaColors)
-        
-        
-#         for i, data in enumerate(fig2.data):
-#             fig.add_trace(data)
-        
-        
-#         Fig = go.FigureWidget(data=fig)
-        
-        
-#         Fig.update_layout(width=600, height=600)
-        
-        
-#         #Right panel with widget and z dependence
-        
-#         textbox = widgets.Dropdown(
-#         description='Fit param.: ',
-#         value='center',
-#         options=[i for i in paramFrame.columns.to_list() if i not in [zcol]]
-#         )
-        
-#         trace = px.line(paramFrame, x=zcol, y='center', markers=True)
-#         trace.data[0].line.color = 'green'
-#         g = go.FigureWidget(data=trace)
-        
-#         g.update_layout(width=400, height=500)
-        
-#         def response(change):
-            
-#             paramYCol = textbox.value
-#             with g.batch_update():
-#                 g.data[0].y = paramFrame[paramYCol]
-#                 g.layout.yaxis.title = paramYCol
-                
-#         textbox.observe(response, names='value')
-        
-#         container = widgets.VBox([textbox, g], 
-#                                  layout = Layout(margin='60px 0 0 0'))
-        
-#         container2 = widgets.HBox([Fig, container])
-        
-        
-#         return container2
-
 def graph_run(scans, xcol, ycol, zcol='Scan', normcol=None):
         
     # if type(ycol) == dict:
@@ -201,7 +71,7 @@ def graph_run(scans, xcol, ycol, zcol='Scan', normcol=None):
         if  zcol=='Scan':
             zval = scanno
         else:
-            baseline = load_scan(scanno, stream_name="baseline")
+            baseline = load_scan(scanno, stream="baseline")
             zval= baseline[zcol].mean()
         zdata = zval*np.ones(len(ydata))
         frame = pd.DataFrame(data={xcol: xdata, ycol: ydata, zcol:zdata})
@@ -241,11 +111,11 @@ def analyze_run(scans, xcol, ycol, zcol='Scan', normcol=None,
                                                   normcol=normcol, getData=True)
             
 
-            baseline = load_scan(scanno, stream_name="baseline")
+            baseline = load_scan(scanno, stream="baseline")
             if zcol=='Scan':
                 zval = scanno
             else:
-                baseline = load_scan(scanno, stream_name="baseline")
+                baseline = load_scan(scanno, stream="baseline")
                 zval= baseline[zcol].mean()
             zdata = zval*np.ones(len(ydata))
             frame = pd.DataFrame(data={xcol: xdata, ycol: ydata, zcol:zdata}) 

mictools/plot_data.py
@@ -1,14 +1,22 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 from .load_data import *
 from .process_data import *
 from .config import *
 
-def plot_flyscan(scanno, detector, roi, roi_type="Intensity", th=None, path=None, **kwargs):
+def plot_flyscan(scanno, 
+                 detector, 
+                 roi=None, 
+                 roi_type="Intensity", 
+                 ch=None, 
+                 th=None, 
+                 path=None,
+                 **kwargs):
     # Load the data
-    X, Y, Z = mesh_roi_data(scanno, detector, roi, roi_type, th, path)
+    X, Y, Z = mesh_detector_data(scanno, detector, roi=roi, roi_type=roi_type, ch=ch, th=th, path=path)
 
     # Plot the data
     fig, ax = plt.subplots()
@@ -20,4 +28,90 @@ def plot_flyscan(scanno, detector, roi, roi_type="Intensity", th=None, path=None
     ax.set_xlabel('X (µm)')
     ax.set_ylabel('Y (µm)')
 
+    ax.set_title(f'Scan {scanno} - {detector} - {roi.name if roi else ch}')
+
     plt.show()
+
+
+def plot_meshed_data(X, Y, Z, ax = None, fig = None):
+
+    if ax is None or fig is None:
+        fig, ax = plt.subplots()
+
+    pcm = ax.pcolormesh(X, Y, Z, shading='auto', **kwargs)
+    fig.colorbar(pcm, ax=ax)
+
+    ax.set_aspect('equal')
+    ax.set_xlabel('X (µm)')
+    ax.set_ylabel('Y (µm)')
+
+    plt.show()
+
+
+def plot_sum_detector_image(scanno,
+                            detector,
+                            summed_image=None,
+                            path=None,
+                            n_workers=None,
+                            log_scale=False,
+                            colorscale="Inferno",
+                            cmin=None,
+                            cmax=None,
+                            width=None,
+                            height=None,
+                            title=None,
+                            **kwargs):
+    '''
+    Plot summed detector image interactively with Plotly.
+
+    Parameters:
+    - scanno: Scan number (int)
+    - detector: Detector name (str)
+    - path: Path to data files (str)
+    - n_workers: Number of workers used by sum_detector_image (int, optional)
+    - log_scale: Plot log10(image + 1) if True (bool)
+    - colorscale: Plotly colorscale name (str)
+    - cmin: Lower limit for color axis (float, optional)
+    - cmax: Upper limit for color axis (float, optional)
+    - width: Figure width in pixels (int, optional)
+    - height: Figure height in pixels (int, optional)
+    - title: Plot title (str, optional)
+    - kwargs: Additional keyword args passed to go.Heatmap
+
+    Returns:
+    - Plotly Figure object
+    '''
+    if summed_image is None:
+        summed_image = sum_detector_image(
+            scanno=scanno,
+            detector=detector,
+            path=path,
+            n_workers=n_workers
+        )
+
+    image_to_plot = np.log10(summed_image + 1) if log_scale else summed_image
+    colorbar_title = "log10(Intensity + 1)" if log_scale else "Intensity"
+    plot_title = title if title is not None else f"Summed Image - Scan {scanno} - {detector}"
+
+    fig = go.Figure(
+        data=go.Heatmap(
+            z=image_to_plot,
+            colorscale=colorscale,
+            zmin=cmin,
+            zmax=cmax,
+            colorbar={"title": colorbar_title},
+            **kwargs
+        )
+    )
+
+    fig.update_layout(
+        title=plot_title,
+        width=width,
+        height=height,
+        xaxis_title="X pixel",
+        yaxis_title="Y pixel"
+    )
+    fig.update_yaxes(scaleanchor="x", scaleratio=1)
+
+    fig.show()
+    return fig

mictools/process_data.py
@@ -13,7 +13,9 @@ from .load_data import load_scan
 from .config import get_path
 from .roi_utils import Roi as ROI
 
-def process_single_file(file, roi):
+
+
+def process_roi_file(file, roi):
     '''
     Process a single HDF5 file and extract ROI data.
     
@@ -56,12 +58,46 @@ def process_single_file(file, roi):
         'times': times
     }
 
-def process_roi_data(scanno, detector, roi, path=None, n_workers=None):
+def process_tetramm_file(file, ch: int):
     '''
-    Loads processed data from a region of interest (ROI) in 
-    flyscan HDF5 files using parallel processing. Returns an Nx4 array 
-    where the first column is timestamps, and the following columns are 
-    intensity in ROI, COM y-position, and COM x-position.
+    Process a single Tetramm HDF5 file and extract channel current.
+    
+    Parameters:
+    - file: Path to HDF5 file (str)
+    - ch: Channel number (int) 
+    
+    Returns:
+    - Dictionary containing intensity, COM positions, and timestamps
+    '''
+
+    if not isinstance(ch, int) or ch < 0 or ch > 3:
+        raise ValueError("Channel number must be an integer between 0 and 4.")
+    
+    with File(file, "r") as f:
+        dset = f["entry/data/data"]
+        data = dset[:, 0, ch-1]
+    
+    return {
+        f'Current {ch}': data,
+    }
+
+
+
+
+
+def process_detector_data(scanno, 
+                     detector, 
+                     roi=None, 
+                     ch=None, 
+                     path=None, 
+                     n_workers=None):
+    '''
+    Loads processed data from a region of interest (ROI) or Tetramm current
+    data in flyscan HDF5 files using parallel processing. 
+    For ROI mode, it returns an Nx4 array where the first column is 
+    timestamps, and the following columns are intensity in ROI, 
+    COM y-position, and COM x-position.
+    For Tetramm mode, it returns an Nx1 array with the tetramm current data.
     
     Parameters:
     - scanno: Scan number (int)
@@ -74,14 +110,12 @@ def process_roi_data(scanno, detector, roi, path=None, n_workers=None):
     '''
 
     path = get_path(path)
-
-    # Check if roi is an instance of roi class
-    if not isinstance(roi, ROI):
-        raise ValueError("roi must be an instance of roi class from roi_utils.py" \
-        "defined from roi_utils.py as roiN = roi(y_start, y_end, x_start, x_end, name=\"roiN\")")
     
     # Check if processed data already exists
-    processed_path = path + f'/Processed/{detector}/Scan_{scanno:04d}_{roi.name}.csv'
+    if roi is not None:
+        processed_path = path + f'/Processed/{detector}/Scan_{scanno:04d}_{roi.name}.csv'
+    elif ch is not None:
+        processed_path = path + f'/Processed/{detector}/Scan_{scanno:04d}_channel_{ch}.csv'
     if os.path.exists(processed_path):
         df = pd.read_csv(processed_path)
         return df
@@ -93,31 +127,52 @@ def process_roi_data(scanno, detector, roi, path=None, n_workers=None):
     # Determine number of workers
     if n_workers is None:
         n_workers = max(1, cpu_count() - 1)
-    
-    # Create partial function with fixed roi parameter
-    process_func = partial(process_single_file, roi=roi)
-    
-    # Process files in parallel
-    with Pool(processes=n_workers) as pool:
-        results = pool.map(process_func, files)
-    
-    # Concatenate results
-    intensity = np.concatenate([r['intensity'] for r in results], axis=0)
-    com_y = np.concatenate([r['com_y'] for r in results], axis=0)
-    com_x = np.concatenate([r['com_x'] for r in results], axis=0)
-    times = np.concatenate([r['times'] for r in results], axis=0)
-    
-    data_array = np.concatenate([
-        times[:, np.newaxis], 
-        intensity[:, np.newaxis],
-        com_y[:, np.newaxis], 
-        com_x[:, np.newaxis]
-    ], axis=1)
 
-    df = pd.DataFrame(data_array, columns=['Timestamp', 
-                                           'Intensity', 
-                                           'COM_Y', 
-                                           'COM_X'])
+    if roi is not None:
+
+        # Check if roi is an instance of roi class
+        if not isinstance(roi, ROI):
+            raise ValueError("roi must be an instance of roi class from roi_utils.py" \
+            "defined from roi_utils.py as roiN = roi(y_start, y_end, x_start, x_end, name=\"roiN\")")
+    
+        # Create partial function with fixed roi parameter
+        process_func = partial(process_roi_file, roi=roi)
+        
+        # Process files in parallel
+        with Pool(processes=n_workers) as pool:
+            results = pool.map(process_func, files)
+        
+        # Concatenate results
+        intensity = np.concatenate([r['intensity'] for r in results], axis=0)
+        com_y = np.concatenate([r['com_y'] for r in results], axis=0)
+        com_x = np.concatenate([r['com_x'] for r in results], axis=0)
+        times = np.concatenate([r['times'] for r in results], axis=0)
+        
+        data_array = np.concatenate([
+            times[:, np.newaxis], 
+            intensity[:, np.newaxis],
+            com_y[:, np.newaxis], 
+            com_x[:, np.newaxis]
+        ], axis=1)
+
+        df = pd.DataFrame(data_array, columns=['Timestamp', 
+                                            'Intensity', 
+                                            'COM_Y', 
+                                            'COM_X'])
+        
+    elif ch is not None:
+
+        # Create partial function with fixed channel parameter
+        process_func = partial(process_tetramm_file, ch=ch)
+        
+        # Process files in parallel
+        with Pool(processes=n_workers) as pool:
+            results = pool.map(process_func, files)
+        
+        # Concatenate results
+        current_data = np.concatenate([r[f'Current {ch}'] for r in results], axis=0)
+        
+        df = pd.DataFrame(current_data, columns=[f'Current {ch}'])
 
     # Ensure processed directory exists
     save_dir = os.path.dirname(processed_path)
@@ -132,7 +187,7 @@ def process_roi_data(scanno, detector, roi, path=None, n_workers=None):
 
 def process_position_data(scanno, 
                           path=None, 
-                          processing_method='basic', 
+                          processing_method='averaging', 
                           th=None, 
                           replace=False):
     '''
@@ -165,6 +220,20 @@ def process_position_data(scanno,
     if processing_method == 'basic':
         x_pos = avg_interf['I15 (X)'].values/np.cos(-1*np.radians(th))
         y_pos = avg_interf['I7 (Y ds)'].values
+    elif processing_method == 'averaging':
+        avg_interf = avg_interf - avg_interf.iloc[0]  # subtract the first point to set it as origin
+        x1 = avg_interf['I15 (X)'].values
+        x2 = avg_interf['I10 (X-us)'].values
+        x3 = avg_interf['I11 (X-ds)'].values
+        y1 = avg_interf['I7 (Y ds)'].values
+        y2 = avg_interf['I8 (Y us-ob)'].values
+        y3 = avg_interf['I9 (Y us-ib)'].values
+        z = avg_interf['I12 (Z)'].values
+        x_avg = (x1 + x2 + x3) / 3 
+        y_avg = (y1 + y2 + y3) / 3
+        x_pos = -np.sqrt(x_avg**2 + z**2)
+        y_pos = y_avg
+
 
     # x_pos /= 1e4  # convert to microns
     # y_pos /= 1e4  # convert to microns
@@ -175,30 +244,36 @@ def process_position_data(scanno,
                        'X_Position': x_pos_um,
                        'Y_Position': y_pos_um})
     
+    df['X_Position'] = -1 * (df['X_Position'] - df['X_Position'].iloc[0])
+    df['Y_Position'] = df['Y_Position'] - df['Y_Position'].iloc[0]
+    
     # Ensure processed directory exists
     processed_path = path + f'/Processed/SOCKETSERVER'
     # print(processed_path)
     # save_dir = os.path.dirname(processed_path)
     os.makedirs(processed_path, exist_ok=True)
     
-    df.to_csv(path + f'/Processed/SOCKETSERVER/scan_{scanno}_position.csv', 
+    df.to_csv(path + f'/Processed/SOCKETSERVER/Scan_{scanno:04d}_position.csv', 
               index=False)
     
     return df
 
-def mesh_roi_data(scanno, detector, roi, roi_type="Intensity", th=None, path=None):
+def mesh_detector_data(scanno, detector, roi=None, roi_type="Intensity", ch=None, th=None, path=None):
     # Load the data
     path = get_path(path)
     if th is None:
         baseline_data = load_scan(scanno, stream='baseline', path=path)
         th = baseline_data['sample_theta'].mean()
     position_data = process_position_data(scanno, th=th, path=path)
-    detector_data = process_roi_data(scanno, detector, roi, path=path)
+    detector_data = process_detector_data(scanno, detector, roi=roi, ch=ch, path=path)
 
     # Align lengths
     min_len = min(len(detector_data), len(position_data))
     position_data = position_data[:min_len]
-    detector_data = detector_data[roi_type][:min_len]
+    if roi is not None:
+        detector_data = detector_data[roi_type][:min_len]
+    elif ch is not None:
+        detector_data = detector_data[f'Current {ch}'][:min_len]
 
     scan_info = get_scan_info(scanno, detector, path)
     ny, nx = scan_info['shape']